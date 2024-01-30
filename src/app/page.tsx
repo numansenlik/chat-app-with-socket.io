b3c062ea50c3665e082d342ad6c30f237a5d7726
Numan Şenlik <nmnsnlk@gmail.com>
2024-01-31 01:30:20 +0300

    changed url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   const [roomId, setroomId] = useState("");
 
   var socket: any;
-  socket = io("http://localhost:3001");
+  socket = io("https://chat-app-with-socket-io.vercel.app");
 
   const handleJoin = () => {
     if (userName !== "" && roomId !== "") {
